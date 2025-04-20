88c3cb0ef7734dc87240d7168464b8d00ad9990a
pranavteja <145261027+Pranavtejar@users.noreply.github.com>
2025-04-20 19:46:17 +0530

    Update quiz_brain.py

--- base/quiz_brain.py
+++ base/quiz_brain.py
@@ -17,8 +17,6 @@ class QuizBrain:
         self.question_number += 1
         q_text = html.unescape(self.current_question.text)
         return f"Q.{self.question_number}: {q_text}"
-        #user_answer = input(f"{self.question_number}: {q_text} : ")
-        #self.check_answer(user_answer)
 
     def check_answer(self, user_answer):
         correct_answer = self.current_question.answer
